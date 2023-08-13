87ac3e21510538c7f17805d4bc2cdb8f84187ad4
Nick Stemp <57169677+nick-stemp@users.noreply.github.com>
2023-08-13 13:03:00 +0100

    PUB-02 Bad code test

---
+++ bad-javascript.js
@@ -0,0 +1,14 @@
+for (;;) {  // Noncompliant; end condition omitted
+    // ...
+  }
+  
+  var j = 0;
+  while (true) { // Noncompliant; constant end condition
+    j++;
+  }
+  
+  var k;
+  var b = true;
+  while (b) { // Noncompliant; constant end condition
+    k++;
+  }
